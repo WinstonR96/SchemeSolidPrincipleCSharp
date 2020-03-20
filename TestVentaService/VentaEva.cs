378bc1b89a195d3ae6d511315c70c542b52cef1a
Winston Jr Rodriguez Stand <wrodriguez@redsis.com>
2020-03-20 12:21:03 -0500

    Agregando test unitarios

---
+++ TestVentaService/VentaEva.cs
@@ -0,0 +1,18 @@
+using GeneradorDeVentas.Models.Eva;
+using GeneradorDeVentas.Resolvers;
+using GeneradorDeVentas.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace TestVentaService
+{
+    public class VentaEva
+    {
+        [Fact]
+        public void Tipo_De_Venta_Es_Eva()
+        {
+            var ventaEva = new VentaService<VentaEvaModel>(new EvaResolver()).GenerarVenta();
+            ventaEva.Should().BeOfType<VentaEvaModel>();
+        }
+    }
+}
